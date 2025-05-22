8efd9b6fdd418009b15901baef8a9624803a2b35
autos1123 <hea514@naver.com>
2025-05-22 20:24:21 +0900

    feat : ItemUser.cs 추가
    - 아이템 사용 기능 추가

---
+++ Assets/Scripts/Items/ItemUser.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 플레이어가 아이템 효과를 실행하는 스크립트
+/// </summary>
+public class ItemUser : MonoBehaviour
+{
+    private PlayerController player;
+
+    private void Awake()
+    {
+        player = GetComponent<PlayerController>();
+    }
+
+    public void UseItem(ItemData item)
+    {
+        switch (item.effectType)
+        {
+            case ItemEffectType.Heal:
+                player.Heal(item.effectValue);
+                break;
+            case ItemEffectType.SpeedBoost:
+                StartCoroutine(SpeedBoost(item.effectValue, item.duration));
+                break;
+            case ItemEffectType.Invincibility:
+                StartCoroutine(Invincibility(item.duration));
+                break;
+        }
+    }
+
+    private IEnumerator SpeedBoost(float multiplier, float duration)
+    {
+        player.moveSpeed *= multiplier;
+        yield return new WaitForSeconds(duration);
+        player.moveSpeed /= multiplier;
+    }
+
+    private IEnumerator Invincibility(float duration)
+    {
+        player.invincible = true;
+        yield return new WaitForSeconds(duration);
+        player.invincible = false;
+    }
+}
