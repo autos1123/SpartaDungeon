4e3b8c2879cdb205ce6e95a237688bc42a4c515e
autos1123 <hea514@naver.com>
2025-05-22 19:48:22 +0900

    feat : ObjectScanner.cs 추가
    - 플레이어가 바라보는 방향으로 Ray를 쏴서 오브젝트를 탐지하는 기능 추가

---
+++ Assets/Scripts/Interactions/ObjectScanner.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ObjectScanner : MonoBehaviour
+{
+    public float scanRange = 5f;  // 조사 거리
+    public LayerMask scanMask;   // 조사 대상 레이어
+    public Camera playerCamera;
+    public InteractableInfoUI ui;
+
+    void Update()
+    {
+        // 카메라 중심에서 전방으로 Raycast
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, scanRange, scanMask))
+        {
+            var target = hit.collider.GetComponent<InspectableObject>();
+            if (target != null)
+            {
+                ui.ShowInfo(target.objectName, target.description);
+                return;
+            }
+        }
+
+        // 아무것도 없으면 숨김
+        ui.HideInfo();
+    }
+}
