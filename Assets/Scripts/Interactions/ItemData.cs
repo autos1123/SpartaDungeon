6096439cffc5fcadbca02d6db5b7445c09967c39
autos1123 <hea514@naver.com>
2025-05-22 20:25:08 +0900

    feat : ItemData.cs 수정
    - 아이템 이펙트와 효과를 적용하기위해 구문 추가

--- Assets/Scripts/Interactions/ItemData.cs
+++ Assets/Scripts/Interactions/ItemData.cs
@@ -6,4 +6,8 @@ public class ItemData : ScriptableObject
     public string itemName;
     [TextArea] public string description;
     public Sprite icon; // 향후 UI에 이미지 넣고 싶을 때 사용
+
+    public ItemEffectType effectType;
+    public float effectValue;   // 회복량, 속도배수 등
+    public float duration;      // 지속시간 (0이면 즉시 효과)
 }
